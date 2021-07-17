667738a01f8ec722c5e1c029f9c7beb576223bfe
hectorcrzprz@gmail.com <hectorcrzprz@gmail.com>
2021-07-17 09:33:35 -0600

    inicio 17 de julio 2021

--- wp-content/themes/agpe/footer.php
+++ wp-content/themes/agpe/footer.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * The template for displaying the footer
  *
@@ -8,16 +9,16 @@
  */
 
 // Exit if accessed directly.
-defined( 'ABSPATH' ) || exit;
+defined('ABSPATH') || exit;
 
-$container = get_theme_mod( 'understrap_container_type' );
+$container = get_theme_mod('understrap_container_type');
 ?>
 
-<?php get_template_part( 'sidebar-templates/sidebar', 'footerfull' ); ?>
+<?php get_template_part('sidebar-templates/sidebar', 'footerfull'); ?>
 
 <div class="wrapper" id="wrapper-footer">
 
-	<div class="<?php echo esc_attr( $container ); ?>">
+	<div class="<?php echo esc_attr($container); ?>">
 
 		<div class="row">
 
@@ -27,13 +28,15 @@ $container = get_theme_mod( 'understrap_container_type' );
 
 					<div class="site-info">
 
-						<?php understrap_site_info(); ?>
+						<?php //understrap_site_info();
+						?>
 
 					</div><!-- .site-info -->
 
 				</footer><!-- #colophon -->
 
-			</div><!--col end -->
+			</div>
+			<!--col end -->
 
 		</div><!-- row end -->
 
@@ -48,4 +51,3 @@ $container = get_theme_mod( 'understrap_container_type' );
 </body>
 
 </html>
-
--- wp-content/themes/agpe/header.php
+++ wp-content/themes/agpe/header.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * The header for our theme
  *
@@ -8,81 +9,75 @@
  */
 
 // Exit if accessed directly.
-defined( 'ABSPATH' ) || exit;
+defined('ABSPATH') || exit;
 
-$container = get_theme_mod( 'understrap_container_type' );
+$container = get_theme_mod('understrap_container_type');
 ?>
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
+
 <head>
-	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta charset="<?php bloginfo('charset'); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 	<link rel="profile" href="http://gmpg.org/xfn/11">
 	<?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?> <?php understrap_body_attributes(); ?>>
-<?php do_action( 'wp_body_open' ); ?>
-<div class="site" id="page">
-
-	<!-- ******************* The Navbar Area ******************* -->
-	<div id="wrapper-navbar">
-
-		<a class="skip-link sr-only sr-only-focusable" href="#content"><?php esc_html_e( 'Skip to content', 'understrap' ); ?></a>
-
-		<nav id="main-nav" class="navbar navbar-expand-md navbar-dark bg-primary" aria-labelledby="main-nav-label">
-
-			<h2 id="main-nav-label" class="sr-only">
-				<?php esc_html_e( 'Main Navigation', 'understrap' ); ?>
-			</h2>
-
-		<?php if ( 'container' === $container ) : ?>
+	<?php do_action('wp_body_open'); ?>
+	<div class="site" id="page">
+		<!-- ******************* The Navbar Area ******************* -->
+		<div class="bg-primary">
 			<div class="container">
-		<?php endif; ?>
+				<div class="row">
+					<div class="col">
+						<ul id="header-datos">
+							<li>
+								<a href="#">
+									<i class="fa fa-phone" aria-hidden="true"></i><span class="small">669 105 14 51</span>
+							</li>
+							</a>
+							<li><a href="#">
+									<i class="fa fa-envelope" aria-hidden="true"></i> <span class="small">contacto@agpecontabilidad.com</span></li>
+							</a>
+							<li><span class="small">Permítenos poner nuestra experiencia a tu servicio. <a href="#"><span class="font-weight-bold text-secundario">¡Contáctanos!</span></a></span></li>
+						</ul>
+					</div>
 
-					<!-- Your site title as branding in the menu -->
-					<?php if ( ! has_custom_logo() ) { ?>
-
-						<?php if ( is_front_page() && is_home() ) : ?>
-
-							<h1 class="navbar-brand mb-0"><a rel="home" href="<?php echo esc_url( home_url( '/' ) ); ?>" itemprop="url"><?php bloginfo( 'name' ); ?></a></h1>
-
-						<?php else : ?>
-
-							<a class="navbar-brand" rel="home" href="<?php echo esc_url( home_url( '/' ) ); ?>" itemprop="url"><?php bloginfo( 'name' ); ?></a>
-
-						<?php endif; ?>
-
-						<?php
-					} else {
-						the_custom_logo();
-					}
-					?>
-					<!-- end custom logo -->
-
-				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="<?php esc_attr_e( 'Toggle navigation', 'understrap' ); ?>">
-					<span class="navbar-toggler-icon"></span>
-				</button>
-
-				<!-- The WordPress Menu goes here -->
-				<?php
-				wp_nav_menu(
-					array(
-						'theme_location'  => 'primary',
-						'container_class' => 'collapse navbar-collapse',
-						'container_id'    => 'navbarNavDropdown',
-						'menu_class'      => 'navbar-nav ml-auto',
-						'fallback_cb'     => '',
-						'menu_id'         => 'main-menu',
-						'depth'           => 2,
-						'walker'          => new Understrap_WP_Bootstrap_Navwalker(),
-					)
-				);
-				?>
-			<?php if ( 'container' === $container ) : ?>
-			</div><!-- .container -->
-			<?php endif; ?>
-
-		</nav><!-- .site-navigation -->
-
-	</div><!-- #wrapper-navbar end -->
+				</div>
+			</div>
+		</div>
+		<header>
+			<div class="container">
+				<div class="row align-items-center no-gutters">
+					<div class="col-sm-12 col-lg-5 text-center text-lg-left">
+						<a href="/" class="logo-agpe">
+							<img src="<?= get_template_directory_uri(); ?>/assets/images/logo_agpe_contabilidad.svg" alt="">
+						</a>
+					</div>
+					<div class="col-sm-12 col-lg-7">
+						<div id="wrapper-navbar" class="mt-md-3 mt-lg-0">
+							<nav id="main-nav" class="navbar navbar-expand-md" aria-labelledby="main-nav-label">
+								<button class="navbar-toggler mt-md-5" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="<?php esc_attr_e('Toggle navigation', 'understrap'); ?>">
+									<span class="navbar-toggler-icon"></span>
+								</button>
+								<?php
+								wp_nav_menu(
+									array(
+										'theme_location'  => 'primary',
+										'container_class' => 'collapse navbar-collapse',
+										'container_id'    => 'navbarNavDropdown',
+										'menu_class'      => 'navbar-nav',
+										'fallback_cb'     => '',
+										'menu_id'         => 'main-menu',
+										'depth'           => 2,
+										'walker'          => new Understrap_WP_Bootstrap_Navwalker()
+									)
+								);
+								?>
+							</nav><!-- .site-navigation -->
+						</div><!-- #wrapper-navbar end -->
+					</div>
+				</div>
+			</div>
+		</header>
