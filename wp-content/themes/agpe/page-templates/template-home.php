04b1e0f71c3bddcacaff460d3225052b4a5120cf
hectorcrzprz@gmail.com <hectorcrzprz@gmail.com>
2021-07-20 00:15:02 -0600

    funcional banner

--- wp-content/themes/agpe/page-templates/template-home.php
+++ wp-content/themes/agpe/page-templates/template-home.php
@@ -14,50 +14,55 @@ defined('ABSPATH') || exit;
 get_header();
 ?>
 
-
 <section id="banner">
-	<div class="container">
-		<div class="row">
-			<div class="col">
-				<div id="banner-contenido">
-					<h1>Despacho Contable en <br>
-						<span>Mazatlán Sinaloa</span>
-					</h1>
-					<p>
-						Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sit
-						mauris donec sit morbi eget aliquam molestie. Molestie
-						libero nam in tempus, faucibus.
-					</p>
-					<a href="#">Ponenerme en contacto</a>
-					<div class="datos-div">
-						<div class="data">
-							<div class="contador-actualizado">
-								<p>
-									Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus totam adipisci assumenda eveniet ut consectetur, fugiat deserunt labore quibusdam vitae. Debitis corporis alias temporibus quaerat, unde consectetur fugiat sequi facilis.
-								</p>
-							</div>
-							<div class="contador-comprometido">
-								<p>
-									Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus totam adipisci assumenda eveniet ut consectetur, fugiat deserunt labore quibusdam vitae. Debitis corporis alias temporibus quaerat, unde consectetur fugiat sequi facilis.
-								</p>
-							</div>
-							<div class="contador-confianza">
-								<p>
-									Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus totam adipisci assumenda eveniet ut consectetur, fugiat deserunt labore quibusdam vitae. Debitis corporis alias temporibus quaerat, unde consectetur fugiat sequi facilis.
-								</p>
-							</div>
-						</div>
+	<div>
+		<div class="container">
+			<div class="row">
+				<div class="col">
+					<div id="datos-banner">
+						<h1>Despacho Contable en <br>
+							<span>Mazatlán Sinaloa</span>
+						</h1>
+						<p>
+							Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sit
+							mauris donec sit morbi eget aliquam molestie. Molestie
+							libero nam in tempus, faucibus.
+						</p>
+						<a href="#">Ponenerme en contacto</a>
 					</div>
-
 				</div>
 			</div>
 		</div>
 	</div>
+	<div class="container">
+		<div class="row">
+			<div class="col">
+				<div id="cards">
+					<div>
+						<div class="contador-actualizado">
+							<p>
+								Aplicamos un modelo de negocio contable vigente, mediante operaciones innovadoras que nos ayudan a mantener actualizada tu empresa para ir un paso adelante de las eventualidades financieras.
+							</p>
+						</div>
+						<div class="contador-comprometido">
+							<p>
+								Tenemos más de 10 años de experiencia en materia fiscal y contable por ello nos hemos convertido en los aliados en contabilidad de múltiples empresas, gracias al compromiso que ponemos en cada proyecto.
+							</p>
+						</div>
+						<div class="contador-confianza">
+							<p>
+								A través de cada uno de nuestros servicios, brindamos una atención puntual y personalizada para garantizar resultados que se reflejan en el crecimiento y la rentabilidad de tu empresa.
+							</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
 </section>
 
-<div class="container">
-
-
+<div class="container bg-warning">
 	<?php
 	for ($i = 0; $i < 100; $i++) {
 		# code...
