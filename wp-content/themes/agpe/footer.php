1606bce66cf49f7fc9a7273d73c9d9844774eff2
hectorcrzprz@gmail.com <hectorcrzprz@gmail.com>
2021-07-23 17:46:37 -0600

    funcional cambios de estilos en el menu

--- wp-content/themes/agpe/footer.php
+++ wp-content/themes/agpe/footer.php
@@ -86,20 +86,31 @@ wp_footer();
 	}
 
 	jQuery(function($) {
-			var scroll_pos = 0;
+		var scroll_pos = 0;
 
-			jQuery(document).scroll(function() {
-					scroll_pos = $(this).scrollTop();
+		jQuery(document).scroll(function() {
+			scroll_pos = $(this).scrollTop();
+			if (scroll_pos > 300) {
+				jQuery("header").addClass('estilos_header_on_scroll');
+			} else {
+				jQuery("header").removeClass('estilos_header_on_scroll');
+			}
+		});
+	});
 
-					if (scroll_pos > 300) {
-						jQuery("header").addClass('estilos_header_on_scroll');
-					} else {
-						jQuery("header").removeClass('estilos_header_on_scroll');
-					}
-				}
 
-			);
-		}
 
-	);
+	var $ = jQuery;
+	jQuery(document).ready(function() {
+		jQuery('.menu-item-has-children').after().click(function() {
+			jQuery(this).toggleClass('ver');
+		});
+
+
+
+	})
+
+	$(window).resize(function() {
+		jQuery('.menu-item-has-children').removeClass('ver');
+	})
 </script>
--- wp-content/themes/agpe/header.php
+++ wp-content/themes/agpe/header.php
@@ -23,60 +23,58 @@ $container = get_theme_mod('understrap_container_type');
 	<?php wp_head(); ?>
 </head>
 
-<body <?php body_class(); ?> <?php understrap_body_attributes(); ?>>
-	<?php do_action('wp_body_open'); ?>
-	<div class="site" id="page">
-		<div class="bg-primary">
-			<div class="container">
-				<div class="row">
-					<div class="col">
-						<ul id="header-datos">
-							<li>
-								<a href="#">
-									<i class="fa fa-phone" aria-hidden="true"></i><span>669 105 14 51</span>
-							</li>
-							</a>
-							<li><a href="#">
-									<i class="fa fa-envelope" aria-hidden="true"></i> <span>contacto@agpecontabilidad.com</span></li>
-							</a>
-							<li><span>Permítenos poner nuestra experiencia a tu servicio. <a href="#">¡<span class="font-weight-bold text-secundario">Contáctanos</span>!</a></span></li>
-						</ul>
-					</div>
-
+<body>
+	<div class="bg-primary">
+		<div class="container">
+			<div class="row">
+				<div class="col">
+					<ul id="header-datos">
+						<li>
+							<a href="#">
+								<i class="fa fa-phone" aria-hidden="true"></i><span>669 105 14 51</span>
+						</li>
+						</a>
+						<li><a href="#">
+								<i class="fa fa-envelope" aria-hidden="true"></i> <span>contacto@agpecontabilidad.com</span></li>
+						</a>
+						<li><span>Permítenos poner nuestra experiencia a tu servicio. <a href="#">¡<span class="font-weight-bold text-secundario">Contáctanos</span>!</a></span></li>
+					</ul>
 				</div>
+
 			</div>
 		</div>
-		<header class="">
-			<div class="container">
-				<div id="wrapper-navbar">
-					<a href="/" class="logo-agpe">
-						<img src="<?= get_template_directory_uri(); ?>/assets/images/agpe.svg" alt="" id="logo-monocolor">
-						<img src="<?= get_template_directory_uri(); ?>/assets/images/logo_agpe_contabilidad.svg" alt="" id="logo-blue">
-					</a>
-					<nav id="main-nav" class="navbar navbar-expand-lg" aria-labelledby="main-nav-label">
-						<button id="btn-toggler" class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="<?php esc_attr_e('Toggle navigation', 'understrap'); ?>">
-							<span class="icon-toggler" onclick="myFunction(this)">
-								<div class="bar1"></div>
-								<div class="bar2"></div>
-								<div class="bar3"></div>
-							</span>
-						</button>
-						<?php
-						wp_nav_menu(
-							array(
-								'theme_location'  => 'primary',
-								'container_class' => 'collapse navbar-collapse',
-								'container_id'    => 'navbarNavDropdown',
-								'menu_class'      => 'navbar-nav',
-								'include_parent' => 1,
-								'fallback_cb'     => '',
-								'menu_id'         => 'main-menu',
-								'depth'           => 2,
-								'walker'          => new Understrap_WP_Bootstrap_Navwalker()
-							)
-						);
-						?>
-					</nav><!-- .site-navigation -->
-				</div><!-- #wrapper-navbar end -->
-			</div>
-		</header>
+	</div>
+	<header class="">
+		<div class="container">
+			<div id="wrapper-navbar">
+				<a href="/" class="logo-agpe">
+					<img src="<?= get_template_directory_uri(); ?>/assets/images/agpe.svg" alt="" id="logo-monocolor">
+					<img src="<?= get_template_directory_uri(); ?>/assets/images/logo_agpe_contabilidad.svg" alt="" id="logo-blue">
+				</a>
+				<nav id="main-nav" class="navbar navbar-expand-lg" aria-labelledby="main-nav-label">
+					<button id="btn-toggler" class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="<?php esc_attr_e('Toggle navigation', 'understrap'); ?>">
+						<span class="icon-toggler" onclick="myFunction(this)">
+							<div class="bar1"></div>
+							<div class="bar2"></div>
+							<div class="bar3"></div>
+						</span>
+					</button>
+					<?php
+					wp_nav_menu(
+						array(
+							'theme_location'  => 'primary',
+							'container_class' => 'collapse navbar-collapse',
+							'container_id'    => 'navbarNavDropdown',
+							'menu_class'      => 'navbar-nav',
+							'include_parent' => 1,
+							'fallback_cb'     => '',
+							'menu_id'         => 'main-menu',
+							'depth'           => 2,
+							'walker'          => new Understrap_WP_Bootstrap_Navwalker()
+						)
+					);
+					?>
+				</nav><!-- .site-navigation -->
+			</div><!-- #wrapper-navbar end -->
+		</div>
+	</header>
