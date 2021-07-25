fb8a3c0b5ddb208ea8b39881b7c8fbc1ef893927
hectorcrzprz@gmail.com <hectorcrzprz@gmail.com>
2021-07-25 13:29:28 -0600

    iniciando pagina de servicios

---
+++ wp-content/themes/agpe/page-templates/template-servicios.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Template Name: Template - Servicios
+ *
+ * Template for displaying a page without sidebar even if a sidebar widget is published.
+ *
+ * @package UnderStrap
+ */
+
+// Exit if accessed directly.
+defined('ABSPATH') || exit;
+
+get_header();
+?>
+
+<?php
+get_template_part('templates_parts_site/contacto');
+get_footer();
