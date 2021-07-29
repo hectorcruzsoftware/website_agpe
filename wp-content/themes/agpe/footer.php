9dae7626415ef8491833061866e71b19e42773d5
hectorcrzprz@gmail.com <hectorcrzprz@gmail.com>
2021-07-28 23:11:55 -0600

    reparado bug de icons after, cambiados por elementos <i>

--- wp-content/themes/agpe/footer.php
+++ wp-content/themes/agpe/footer.php
@@ -85,37 +85,3 @@ wp_footer();
 </body>
 
 </html>
-<script type="text/javascript">
-	function myFunction(x) {
-		x.classList.toggle("change");
-	}
-
-	jQuery(function($) {
-		var scroll_pos = 0;
-
-		jQuery(document).scroll(function() {
-			scroll_pos = $(this).scrollTop();
-			if (scroll_pos > 300) {
-				jQuery("header").addClass('estilos_header_on_scroll');
-			} else {
-				jQuery("header").removeClass('estilos_header_on_scroll');
-			}
-		});
-	});
-
-
-
-	var $ = jQuery;
-	jQuery(document).ready(function() {
-		jQuery('.menu-item-has-children').after().click(function() {
-			jQuery(this).toggleClass('ver');
-		});
-
-
-
-	})
-
-	$(window).resize(function() {
-		jQuery('.menu-item-has-children').removeClass('ver');
-	})
-</script>
