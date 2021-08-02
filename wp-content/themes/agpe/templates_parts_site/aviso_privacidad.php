dac52227f262e8378129edf402cb4290e68a76f2
hectorcrzprz@gmail.com <hectorcrzprz@gmail.com>
2021-08-02 15:51:45 -0600

    modal de aviso de privacidad funcional

---
+++ wp-content/themes/agpe/templates_parts_site/aviso_privacidad.php
@@ -0,0 +1,30 @@
+<section class="aviso-privacidad aviso-modal">
+	<div class="contenido">
+		<div class="container">
+			<div class="row">
+				<div class="col-12">
+					<p>MarketHax con domicilio en calle Aguilar y Maya 106 pte int.6, colonia Alameda, ciudad Celaya, municipio o delegación Celaya, c.p. 38050, en la entidad de Guanajuato, país México, y portal de internet www.markethax.com, es el responsable del uso y protección de sus datos personales, y al respecto le informamos lo siguiente:</p>
+					<p>¿Para qué fines utilizaremos sus datos personales?</p>
+					<p>De manera adicional, utilizaremos su información personal para las siguientes finalidades secundarias que no son necesarias para el servicio solicitado, pero que nos permiten y facilitan brindarle una mejor atención:</p>
+					<ul>
+						<li>
+							Envío de información sobre servicios de la empresa.
+						</li>
+						<li>
+							Mercadotecnia y publicidad.
+						</li>
+					</ul>
+					<p>
+						En caso de que no desee que sus datos personales sean tratados para estos fines secundarios, desde este momento usted nos puede comunicar lo anterior a través del siguiente mecanismo: Enviar mensaje en markethax.com/contacto. La negativa para el uso de sus datos personales para estas finalidades no podrá ser un motivo para que le neguemos los servicios y productos que solicita o contrata con nosotros.
+					</p>
+					<p>
+						¿Dónde puedo consultar el aviso de privacidad integral?
+					</p>
+					<p>
+						Para conocer mayor información sobre los términos y condiciones en que serán tratados sus datos personales, como los terceros con quienes compartimos su información personal y la forma en que podrá ejercer sus derechos ARCO, puede consultar el aviso de privacidad integral en: Petición vía correo electrónico hola @ markethax.com
+					</p>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
--- wp-content/themes/agpe/templates_parts_site/contacto.php
+++ wp-content/themes/agpe/templates_parts_site/contacto.php
@@ -74,7 +74,7 @@ defined('ABSPATH') || exit;
 								</div>
 								<div class="col-sm-12 col-lg-6">
 									<div class="input-check">
-										<input type="checkbox" id="chkTerminos" name="chkTerminos" value="1" <?= WP_DEBUG ? 'checked' : ''; ?>> <label> He leído el <a href="#">Aviso de privacidad</a> y deseo recibir información.</label><br>
+										<input type="checkbox" id="chkTerminos" name="chkTerminos" value="1" <?= WP_DEBUG ? 'checked' : ''; ?>> <label> He leído el <a href="#" data-toggle="modal" data-target="#ModalAviso">Aviso de privacidad</a> y deseo recibir información.</label><br>
 									</div>
 								</div>
 								<div class="col-sm-12">
@@ -132,6 +132,25 @@ defined('ABSPATH') || exit;
 						</div>
 					</div>
 					<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3665.4742894170977!2d-106.42007028502786!3d23.2622099848323!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x869f53dc2d831439%3A0x18ee8659a76e9d3b!2sAGPE%20CONTABILIDAD%20Y%20ADMINISTRACION!5e0!3m2!1ses-419!2smx!4v1626935907243!5m2!1ses-419!2smx" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+				</div>
+			</div>
+
+			<!-- Modal -->
+			<div class="modal fade" id="ModalAviso" tabindex="-1" role="dialog" aria-hidden="true">
+				<div class="modal-dialog  modal-lg" role="document">
+					<div class="modal-content">
+						<div class="modal-header">
+							<h5 class="modal-title">Aviso de privacidad</h5>
+							<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+								<span aria-hidden="true">&times;</span>
+							</button>
+						</div>
+						<div class="modal-body">
+							<?php
+							get_template_part('templates_parts_site/aviso_privacidad');
+							?>
+						</div>
+					</div>
 				</div>
 			</div>
 		</div>
