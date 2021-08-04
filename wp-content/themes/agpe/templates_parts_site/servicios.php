37e66447c77aa9e0ab943824fe8b67301ad86f65
hectorcrzprz@gmail.com <hectorcrzprz@gmail.com>
2021-08-04 17:20:59 -0600

    servicios en espera de contenido

--- wp-content/themes/agpe/templates_parts_site/servicios.php
+++ wp-content/themes/agpe/templates_parts_site/servicios.php
@@ -3,17 +3,13 @@
 defined('ABSPATH') || exit;
 ?>
 <section id="lista-servicios">
-
 	<?php
-
 	/**filtrnado los datos de los servicios */
-
-
 	//obteniendo el menu
 	$menu_name = 'top-menu';
 	$array_menu = wp_get_nav_menu_items($menu_name);
 
-	$uri_menu = "/servicios";
+	$uri_menu = "/servicios-de-contabilidad-en-mazatlan";
 
 	//URI actual
 	$current_uri = '/' . str_replace('/', '', $_SERVER['REQUEST_URI']);
@@ -31,26 +27,7 @@ defined('ABSPATH') || exit;
 					if ($servicio_num == 3 && $current_uri != $uri_menu) {
 						break;
 					}
-					switch ($item_menu->url) {
-						case '/servicio-de-contabilidad-en-mazatlan-sinaloa':
-							$imagen = 'servicio1';
-							break;
-						case '/consultoria-administrativa':
-							$imagen = 'servicio2';
-							break;
-						case '/consultoria-fiscal':
-							$imagen = 'servicio3';
-							break;
-						case '/regulacion-de-obligaciones-fiscales':
-							$imagen = 'servicio4';
-							break;
-						case '/soporte-en-sistemas-contpaq':
-							$imagen = 'servicio5';
-							break;
-						default:
-							$imagen = 'servicio6';
-							break;
-					}
+					$imagen = str_replace('/', '', $item_menu->url);
 					//print_r($item_menu);
 	?>
 					<div>
@@ -60,7 +37,30 @@ defined('ABSPATH') || exit;
 							<?php
 							if ($current_uri == $uri_menu) {
 							?>
-								<p>Somos contadores públicos determinados. Por ello, buscamos que Despacho Contable México sea reconocido a nivel nacional, no sólo por ser prestadores de servicios en materia contable, sino por ser los mejores aliados para tu negocio o empresa, porque nos satisface crecer de la mano de nuestros clientes.</p>
+								<p>
+									<?php
+									switch (!home_url() . $item_menu->url . '/') {
+										case (get_permalink(ID_SERVICIO_CONTABILIDAD)):
+											echo 'Mensjae';
+											break;
+										case (get_permalink(ID_SERVICIO_CONSULTORIA_ADMINISTRATIVA)):
+											echo 'Mensjae';
+											break;
+										case (get_permalink(ID_SERVICIO_CONSULTORIA_FISCAL)):
+											echo 'Mensjae';
+											break;
+										case (get_permalink(ID_SERVICIO_REGULACION_FISCAL)):
+											echo 'Mensjae';
+											break;
+										case (get_permalink(ID_SERVICIO_SOPORTE_CONTPAQ)):
+											echo 'Mensjae';
+											break;
+										default:
+											echo 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi minus repudiandae architecto, sunt recusandae officiis explicabo reiciendis voluptas est. Magni necessitatibus, eum qui optio molestiae tenetur tempore amet doloribus dolores.';
+											break;
+									}
+									?>
+								</p>
 							<?php
 							} else {
 							?>
@@ -70,7 +70,7 @@ defined('ABSPATH') || exit;
 										echo $item_menu->description;
 									} else {
 									?>
-										Ingeniero electrónico, de sistemas y magister en ingeniería Ingeniero electrónico, de sistemas y magister en ingeniería
+										-
 									<?php
 									}
 									?>
