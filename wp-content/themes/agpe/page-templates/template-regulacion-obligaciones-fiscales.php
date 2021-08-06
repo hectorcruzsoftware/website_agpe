036e4847edba529519df985576774cc30321bc5f
hectorcrzprz@gmail.com <hectorcrzprz@gmail.com>
2021-08-05 22:43:37 -0600

    funcional SEO de regulación fiscal

--- wp-content/themes/agpe/page-templates/template-regulacion-obligaciones-fiscales.php
+++ wp-content/themes/agpe/page-templates/template-regulacion-obligaciones-fiscales.php
@@ -18,8 +18,12 @@ get_header();
 		<div class="container">
 			<div class="row">
 				<div class="col">
-					<h1>Servicio de Contabilidad en Mazatlán</h1>
-					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias voluptatibus velit dicta minima quo iure sed debitis nemo commodi quos voluptas, molestias ut ad magni! Laudantium et commodi repellendus ab.</p>
+					<h1>
+						Regulación fiscal <br> en Mazatlán Sinaloa
+					</h1>
+					<p>
+						Asesoría en <strong>regulación fiscal</strong>, evita multas y recargos a tu empresa por incumplimientos ante autoridades fiscales.
+					</p>
 					<a href="<?= get_the_permalink(ID_CONTACTO) ?>" class="btn-theme-big-icon">Ponerme en contacto</a>
 				</div>
 			</div>
@@ -30,64 +34,100 @@ get_header();
 			<div class="row">
 				<div class="col-12 col-sm-12 col-lg-8">
 					<div class="card-servicio">
-						<h2>¿Qué es este servicio?</h2>
+						<h2>¿Qué es la regulación fiscal?</h2>
 						<p>
-							Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore iure quod perferendis laboriosam voluptatum minus, facilis dolores mollitia repellat officiis ipsa. Eius voluptates modi perferendis nobis, vel exercitationem tenetur earum?
+							En pocas palabras, son todos los medios que las autoridades como el <a href="https://www.sat.gob.mx/home" target="_blank" rel="noopener noreferrer">SAT</a>,<a href="http://www.imss.gob.mx/" target="_blank" rel="noopener noreferrer"> IMSS</a>,<a href="https://portalmx.infonavit.org.mx/wps/portal/infonavit.web/trabajadores/!ut/p/z1/hY-9DoIwEICfxYGVHj8ScKsGi5VAHIjYxYDBggFKAOH1BWUxEbzt7r7vfhBDIWJl1GU8ajNRRvmQX5hxVU0AsgXFI6ZjAt7bB9tyNZUogM7_ADa0DQLg7HTF8y1qwOnoEV83KKWuNvkLwOjDTOBxP3sjcxcEygQszKCI8VzEn3dxGWsmR6xO7kmd1PKzHspp21bNRgIJ-r6XuRA8T-SbKCT4paSiaVH4TaKqCEJ4rPPOxasXvmcXCw!!/dz/d5/L2dBISEvZ0FBIS9nQSEh/" target="_blank" rel="noopener noreferrer"> INFONAVIT</a>, etc. ofrecen a sus contribuyentes para ponerse al corriente con deudas de impuestos o cuotas. Estos procesos se puede llevar a cabo de forma voluntaria o de manera obligatoria.
 						</p>
 						<p>
-							Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore iure quod perferendis laboriosam voluptatum minus, facilis dolores mollitia repellat officiis ipsa. Eius voluptates modi perferendis nobis, vel exercitationem tenetur earum?
+							<strong>Voluntariamente</strong>: El contribuyente mediante su contador corrige sus errores en las declaraciones y paga sus adeudos resultantes en sus nuevas declaraciones. Así, puede en ocasiones evitar multas u otras responsabilidades legales.
 						</p>
 						<p>
-							Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore iure quod perferendis laboriosam voluptatum minus, facilis dolores mollitia repellat officiis ipsa. Eius voluptates modi perferendis nobis, vel exercitationem tenetur earum?
+							<strong>Obligatoriamente</strong> Cuando la autoridad notifica a tu empresa que ha encontrado irregularidades en sus declaraciones. Para este caso, se puede esperar incluso recibir multas y sanciones por no cubrir sus pagos a tiempo.
 						</p>
 					</div>
 				</div>
 				<div class="col-12 col-sm-12 col-lg-4">
-					<img src="<?= get_template_directory_uri(); ?>/assets/images/servicio1.jpg" alt="" class="img-infografia">
+					<img src="<?= get_template_directory_uri(); ?>/assets/images/regulacion-fiscal-en-mazatlan-sinaloa.jpg" alt="Regulación fiscal en Mazatlán Sinaloa" class="img-infografia">
 				</div>
 			</div>
-
 
 			<div class="row">
 				<div class="col-12 col-sm-12 col-lg-8">
 					<div class="preguntas">
-						<h3>Preguntas frecuentes</h3>
-						<?php
-						for ($i = 0; $i < 7; $i++) {
-						?>
-
-							<div class="pregunta">
-								<h5>
-									<?= $i + 1; ?>. sobre el servicio
-									<span data-toggle="collapse" data-target="#collapseExample<?= $i; ?>" aria-expanded="false" aria-controls="collapseExample<?= $i; ?>">
-										<i class="fa fa-chevron-right" aria-hidden="true"></i>
-									</span>
-								</h5>
-								<div class="collapse" id="collapseExample<?= $i; ?>">
-									<div class="">
-										Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident.
-									</div>
+						<h3>Preguntas frecuentes sobre este servicio</h3>
+						<div class="pregunta">
+							<h4 data-toggle="collapse" data-target="#pregunta-frecuente-1" aria-expanded="false" aria-controls="pregunta-frecuente-1">
+								¿Cuándo necesito este servicio para mi empresa?
+								<span data-toggle="collapse" data-target="#pregunta-frecuente-1" aria-expanded="false" aria-controls="pregunta-frecuente-1">
+									<i class="fa fa-chevron-right" aria-hidden="true"></i>
+								</span>
+							</h4>
+							<div class="collapse" id="pregunta-frecuente-1">
+								<div class="">
+									Lo ideal es que esté siempre presente en tu empresa. Pero si no es el caso, puedes buscarnos cuando tu empresa reciba alguna notificación de las autoridades para realizar aclaraciones en tus pagos de impuestos, seguro, Infonavit, etc.
 								</div>
 							</div>
-						<?php
-						}
-						?>
-					</div>
-
-
-				</div>
-				<div class="col-12 col-sm-12 col-lg-4">
-					<section class="card-contador">
-						<h5>Más información</h5>
-						<div class="datos">
-							<img src="<?= get_template_directory_uri(); ?>/assets/images/benjamin.jpg" alt="">
-							<div class="info">
-								<p>L.C.P. Benjamin Aguirre <a href="#">Contactar</a></p>
-								<p>Ingeniero electrónico, de sistemas y magister en ingeniería de sistemas. Profesor catedrático en ciencias de la computación, los</p>
+						</div>
+						<div class="pregunta">
+							<h4 data-toggle="collapse" data-target="#pregunta-frecuente-2" aria-expanded="false" aria-controls="pregunta-frecuente-2">
+								¿Qué beneficios obtengo de hacerlo voluntariamente?
+								<span data-toggle="collapse" data-target="#pregunta-frecuente-2" aria-expanded="false" aria-controls="pregunta-frecuente-2">
+									<i class="fa fa-chevron-right" aria-hidden="true"></i>
+								</span>
+							</h4>
+							<div class="collapse" id="pregunta-frecuente-2">
+								<div class="">
+									En algunos casos y dependiendo de las circunstancias, puede que la autoridad no aplique multas o te haga acreedor a conflictos legales, pero si deberás cubrir tus adeudos pendientes.
+								</div>
 							</div>
 						</div>
 
-					</section>
+						<div class="pregunta">
+							<h4 data-toggle="collapse" data-target="#pregunta-frecuente-3" aria-expanded="false" aria-controls="pregunta-frecuente-3">
+								¿Qué consecuencias existen de ignorar esta situación en mi empresa?
+								<span data-toggle="collapse" data-target="#pregunta-frecuente-3" aria-expanded="false" aria-controls="pregunta-frecuente-3">
+									<i class="fa fa-chevron-right" aria-hidden="true"></i>
+								</span>
+							</h4>
+							<div class="collapse" id="pregunta-frecuente-3">
+								<div class="">
+									La autoridad puede auditar tu empresa si detecta irregularidades y te aplicará multas por evasión de impuestos y otros cargos. Por lo general son multas altas que pueden afectar la estabilidad de tu negocio.
+								</div>
+							</div>
+						</div>
+
+						<div class="pregunta">
+							<h4 data-toggle="collapse" data-target="#pregunta-frecuente-5" aria-expanded="false" aria-controls="pregunta-frecuente-5">
+								¿Cómo evito esta situación?
+								<span data-toggle="collapse" data-target="#pregunta-frecuente-5" aria-expanded="false" aria-controls="pregunta-frecuente-5">
+									<i class="fa fa-chevron-right" aria-hidden="true"></i>
+								</span>
+							</h4>
+							<div class="collapse" id="pregunta-frecuente-5">
+								<div class="">
+									Acércate con tu equipo de contabilidad y lleven el registro de sus operaciones de manera ordenada así como sus pagos y cuotas. Esto te da la seguridad como empresa que estás siempre cumpliendo con la normatividad.
+								</div>
+							</div>
+						</div>
+						<div class="pregunta">
+							<h4 data-toggle="collapse" data-target="#pregunta-frecuente-6" aria-expanded="false" aria-controls="pregunta-frecuente-6">
+								¿Quiénes pueden asesorarme si tengo dudas al respecto?
+								<span data-toggle="collapse" data-target="#pregunta-frecuente-6" aria-expanded="false" aria-controls="pregunta-frecuente-6">
+									<i class="fa fa-chevron-right" aria-hidden="true"></i>
+								</span>
+							</h4>
+							<div class="collapse" id="pregunta-frecuente-6">
+								<div class="">
+									Tu contador es tu intermediario entre tu y las autoridades fiscales. En <strong>AGPE Contabilidad</strong>, informamos a nuestros clientes sobre su status ante SAT, IMSS, Infonavit, etc. en todo momento. Por lo cual, les damos la tranquilidad que necesitan para operar sus negocios sin problemas.
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="col-12 col-sm-12 col-lg-4">
+					<?php
+					get_template_part('templates_parts_site/card_contador');
+					?>
 					<section id="otros-servicios">
 						<h2>Más Servicios</h2>
 						<?php
@@ -96,7 +136,6 @@ get_header();
 					</section>
 				</div>
 			</div>
-
 
 		</div>
 	</div>
