1d0d86e54057702747ff4acc0de129e7777538db
hectorcrzprz@gmail.com <hectorcrzprz@gmail.com>
2021-08-18 15:25:50 -0600

    inicio del 18 de agosto 2021

--- wp-content/themes/agpe/page-templates/template-soporte-sistemas-contpaq.php
+++ wp-content/themes/agpe/page-templates/template-soporte-sistemas-contpaq.php
@@ -401,43 +401,93 @@ get_header();
 			<div class="row">
 				<div class="col-12 col-sm-12 col-lg-8">
 					<div class="preguntas">
-						<h3>Preguntas frecuentes</h3>
-						<?php
-						for ($i = 0; $i < 7; $i++) {
-						?>
-
-							<div class="pregunta">
-								<h5>
-									<?= $i + 1; ?>. sobre el servicio
-									<span data-toggle="collapse" data-target="#collapseExample<?= $i; ?>" aria-expanded="false" aria-controls="collapseExample<?= $i; ?>">
-										<i class="fa fa-chevron-right" aria-hidden="true"></i>
-									</span>
-								</h5>
-								<div class="collapse" id="collapseExample<?= $i; ?>">
-									<div class="">
-										Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident.
-									</div>
+						<h3>Preguntas comunes sobre CONTPAQi</h3>
+						<div class="pregunta">
+							<h4 data-toggle="collapse" data-target="#pregunta-frecuente-1" aria-expanded="false" aria-controls="pregunta-frecuente-1">
+								¿Cuándo necesito asesoría contable?
+								<span data-toggle="collapse" data-target="#pregunta-frecuente-1" aria-expanded="false" aria-controls="pregunta-frecuente-1">
+									<i class="fa fa-chevron-right" aria-hidden="true"></i>
+								</span>
+							</h4>
+							<div class="collapse" id="pregunta-frecuente-1">
+								<div class="">
+									Cuando vas a iniciar un negocio y no sabes que requisitos te pide la autoridad para iniciar operaciones. Esto varía según tu actividad o giro de negocio.
 								</div>
 							</div>
-						<?php
-						}
-						?>
-					</div>
-
-
-				</div>
-				<div class="col-12 col-sm-12 col-lg-4">
-					<section class="card-contador">
-						<h5>Más información</h5>
-						<div class="datos">
-							<img src="<?= get_template_directory_uri(); ?>/assets/images/benjamin.jpg" alt="">
-							<div class="info">
-								<p>L.C.P. Benjamin Aguirre <a href="#">Contactar</a></p>
-								<p>Ingeniero electrónico, de sistemas y magister en ingeniería de sistemas. Profesor catedrático en ciencias de la computación, los</p>
+						</div>
+						<div class="pregunta">
+							<h4 data-toggle="collapse" data-target="#pregunta-frecuente-2" aria-expanded="false" aria-controls="pregunta-frecuente-2">
+								¿Cómo me registro mi negocio en el SAT?
+								<span data-toggle="collapse" data-target="#pregunta-frecuente-2" aria-expanded="false" aria-controls="pregunta-frecuente-2">
+									<i class="fa fa-chevron-right" aria-hidden="true"></i>
+								</span>
+							</h4>
+							<div class="collapse" id="pregunta-frecuente-2">
+								<div class="">
+									Una vez que ya sabes que régimen fiscal te conviene, se debe <a href="https://citas.sat.gob.mx/citasat/agregarcita.aspx" target="_blank" rel="noopener noreferrer"><strong>agendar una cita en el SAT</strong></a> para obtener la firma electrónica. Esta firma es la base para realizar otros trámites y poder emitir facturas a tus clientes. </div>
 							</div>
 						</div>
 
-					</section>
+						<div class="pregunta">
+							<h4 data-toggle="collapse" data-target="#pregunta-frecuente-3" aria-expanded="false" aria-controls="pregunta-frecuente-3">
+								¿Qué régimen fiscal me conviene más?
+								<span data-toggle="collapse" data-target="#pregunta-frecuente-3" aria-expanded="false" aria-controls="pregunta-frecuente-3">
+									<i class="fa fa-chevron-right" aria-hidden="true"></i>
+								</span>
+							</h4>
+							<div class="collapse" id="pregunta-frecuente-3">
+								<div class="">
+									Depende de tu actividad, profesión y tamaño de negocio, para ello el SAT tiene un <a href="https://www.sat.gob.mx/consulta/09788/emprendedor-conoce-los-regimenes-fiscales" target="_blank" rel="noopener noreferrer"><strong>catálogo</strong></a> de regímenes fiscales donde puedes identificar tu caso en particular. Es recomendable que esto lo hagas con asesoría contable profesional. </div>
+							</div>
+						</div>
+
+						<div class="pregunta">
+							<h4 data-toggle="collapse" data-target="#pregunta-frecuente-5" aria-expanded="false" aria-controls="pregunta-frecuente-5">
+								¿Qué pasa si no registro mi negocio ante el SAT?
+								<span data-toggle="collapse" data-target="#pregunta-frecuente-5" aria-expanded="false" aria-controls="pregunta-frecuente-5">
+									<i class="fa fa-chevron-right" aria-hidden="true"></i>
+								</span>
+							</h4>
+							<div class="collapse" id="pregunta-frecuente-5">
+								<div class="">
+									Primero que nada estás limitando el crecimiento de tu empresa por que no podrás emitir facturas a tus clientes. Por otro lado el SAT te invitará a regularizar tu negocio e incluso aplicará las multas y clausuras correspondientes. <strong>¡Todos debemos estar registrados si queremos crecer!</strong>
+								</div>
+							</div>
+						</div>
+
+						<div class="pregunta">
+							<h4 data-toggle="collapse" data-target="#pregunta-frecuente-6" aria-expanded="false" aria-controls="pregunta-frecuente-6">
+								¿Tiene algún costo registrarme ante el SAT?
+								<span data-toggle="collapse" data-target="#pregunta-frecuente-6" aria-expanded="false" aria-controls="pregunta-frecuente-6">
+									<i class="fa fa-chevron-right" aria-hidden="true"></i>
+								</span>
+							</h4>
+							<div class="collapse" id="pregunta-frecuente-6">
+								<div class="">
+									Todos los trámites ante el SAT son gratis. Al contratar la asesoría contable de <strong>AGPE Contabilidad</strong> nos encargaremos de que tu trámite sea lo más fácil y rápido posible.
+								</div>
+							</div>
+						</div>
+
+						<div class="pregunta">
+							<h4 data-toggle="collapse" data-target="#pregunta-frecuente-4" aria-expanded="false" aria-controls="pregunta-frecuente-4">
+								¿Qué debo pagar cuando ya estoy registrado en el SAT?
+								<span data-toggle="collapse" data-target="#pregunta-frecuente-4" aria-expanded="false" aria-controls="pregunta-frecuente-4">
+									<i class="fa fa-chevron-right" aria-hidden="true"></i>
+								</span>
+							</h4>
+							<div class="collapse" id="pregunta-frecuente-4">
+								<div class="">
+									Una vez que ya estás registrado, necesitas los servicios de un contador para que lleve a cabo el control de tus operaciones y haga tu declaración de impuestos. Las tarifas de este servicio varían según tu régimen fiscal y tus impuestos se calculan en base a tus operaciones realizadas durante tu periodo de operación.
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="col-12 col-sm-12 col-lg-4">
+					<?php
+					get_template_part('templates_parts_site/card_contador');
+					?>
 					<section id="otros-servicios">
 						<h2>Más Servicios</h2>
 						<?php
